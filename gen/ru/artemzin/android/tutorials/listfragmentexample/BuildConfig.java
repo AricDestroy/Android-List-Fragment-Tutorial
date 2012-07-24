86444134a3509e1a2a99f95ed1c43ee486df8932
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2012-07-24 13:33:59 +0600

    Created project

---
+++ gen/ru/artemzin/android/tutorials/listfragmentexample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ru.artemzin.android.tutorials.listfragmentexample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
